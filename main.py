166e54079556f5f1e308d3e852175dd30d7b9ae9
ikeikebab <thanapatikekwanmuang@gmail.com>
2024-04-17 18:50:20 +0200

    hej

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os import listdir
 from os.path import isfile, join
 pygame.init()
 
-pygame.display.set_caption("Hopper")
+pygame.display.set_caption("Platform Game")
 
 BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
@@ -14,8 +14,21 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i + width, j + height]
+            tiles.append(pos)
+
+    return tiles, image 
+
 def main(window):
-    clock = pygame.time.Clock()
+    clock = pygame.time.Clock() 
+    background, bg_image = get.background("Gray.png")
 
     run = True
     while run:
