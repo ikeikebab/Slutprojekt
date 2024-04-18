332f721254448b25fbf49f2ff8411ef020e55984
ikeikebab <thanapatikekwanmuang@gmail.com>
2024-04-18 08:24:50 +0200

    gray

--- main.py
+++ main.py
@@ -278,7 +278,7 @@ def handle_move(player, objects):
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background("Blue.png")
+    background, bg_image = get_background("Gray.png")
 
     block_size = 96
 
